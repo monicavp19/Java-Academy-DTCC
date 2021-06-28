2e2262e908abfb355fe42d55c1721fd5ba9a25f6
Monica <monicavp19@yahoo.com>
2021-06-28 17:30:02 -0400

    Added response

--- projects/assignment_2/data_types/BoxedDataTypes.java
+++ projects/assignment_2/data_types/BoxedDataTypes.java
@@ -9,5 +9,12 @@ public class BoxedDataTypes {
         Integer i_1 = 0;
         // versus
         int i_2 = 0;
+
+        boolean check1 = (i_2 == i_1.intValue());
+        System.out.println(check1);
+        boolean check2 = i_2 == i_1;
+        System.out.println(check2);
+        boolean check3 = i_1.toString().equals(i_2);
+        System.out.println(check3);
     }
 }
