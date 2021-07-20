3ebeee350969c9e04d43e5d61dec4494bf8df12e
Monica <monicavp19@yahoo.com>
2021-07-20 00:56:00 -0400

    Changes done.

--- projects/src/main/java/assignment_3/item_package/driver.java
+++ projects/src/main/java/assignment_3/item_package/driver.java
@@ -1,6 +1,12 @@
 package assignment_3.item_package;
 
 public class driver {
+    private int price;
+    private String description;
+    private String name;
+    private String type;
+
+
     public static void main (String args []) {
         /*
             This can be where you attempt to test constructing your objects!
@@ -9,6 +15,53 @@ public class driver {
          */
 
         // Example construction of root class Object
-        Object o = new Object();
+        driver car= new driver();
+        car.setPrice(20000);
+        car.setDescription("White car");
+        car.setName("Honda");
+        car.setType("Sedan");
+        System.out.println(car);
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "driver{" +
+                "price=" + price +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
     }
 }
