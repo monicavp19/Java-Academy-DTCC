c75e40235438b42bb1fd2988ddaabc8ec0c1d1b1
Monica <monicavp19@yahoo.com>
2021-07-19 23:25:26 -0400

    Added changes

--- projects/src/main/java/assignment_3/people_package/Person.java
+++ projects/src/main/java/assignment_3/people_package/Person.java
@@ -1,4 +1,5 @@
 package assignment_3.people_package;
+import java.util.Random;
 /*
     This is the class you need to work on
     If you were to create a class from scratch, please remember that the name of the class should match
@@ -9,4 +10,56 @@ package assignment_3.people_package;
  */
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+    public void setName(String n){
+         this.name=n;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args){
+        Random r=new Random();
+        for(int i=0; i<=10; i++){
+            int randomId = r.nextInt(1000);
+            int randomAge= r.nextInt(99);
+
+            Person M= new Person();
+            M.setName("A-"+i);
+            M.setAge(randomAge);
+            M.setId(randomId);
+            System.out.println("value "+ M.toString());
+        }
+
+
+
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                '}';
+    }
 }
